f6c8b9c85b649d93f80a5ca8772fefd31c3f28bd
sophiasussa <sophiasussa@outlook.com>
2025-10-06 20:38:56 -0300

    feat: add home header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,17 @@
+import Image from 'next/image';
+import logo from '../assets/logo.png';
+import { Menu, CircleUserRound } from 'lucide-react';
+
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
-      <h1 className="text-4xl font-bold text-gray-800 mb-6">Bem-vindo ao Nina!</h1>
-      <p className="text-lg text-gray-600 mb-6">
-        Gerencie seus clientes, pedidos, insumos e finanças de forma simples e eficiente.
-      </p>
-
-      <div className="flex gap-4">
-        <button className="px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 transition">
-          Entrar
-        </button>
-        <button className="px-6 py-3 bg-gray-200 text-gray-800 font-semibold rounded-lg hover:bg-gray-300 transition">
-          Saiba Mais
-        </button>
-      </div>
-    </main>
+    <section className="min-h-screen bg-gray-50">
+      <header className="flex w-full px-28 py-3 bg-white items-center justify-between">
+        <Image src={logo} alt="Logo do Software gerenciador de confeitaria Nina" height={50} width={180} priority/>
+        <div className='flex justify-end gap-8'>
+          <Menu className='text-gray-700'/>
+          <CircleUserRound className='text-gray-700'/>
+        </div>
+      </header>
+    </section>  
   );
 }
