9dd5da6a9187735975e2ceee58fe8605b90dfe65
sophiasussa <sophiasussa@outlook.com>
2026-04-23 13:50:37 -0300

    chore: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="bg-primary min-h-screen flex items-center justify-center">
+      <h1 className="text-white text-3xl font-bold">
+        Funcionando 🎉
+      </h1>
+    </div>
+  )
+}
